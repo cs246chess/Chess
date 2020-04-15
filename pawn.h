9731651ef504855ad59ec501275e5b367dd48468
Atanyante <51100049+Atanyante@users.noreply.github.com>
2020-04-15 17:02:43 -0400

    Added Col, Row and Color

--- pawn.h
+++ pawn.h
@@ -4,6 +4,9 @@
 class piece;
 
 class Pawn : public piece {
-  bool validMove(int col, int row);
-  void move(int col, int row);
-}
+  bool validMove(int col, int row) const override;
+  void move(int col, int row) const override;
+  int getCol() const override;
+  int getRow() const override;
+  char getColor() const override;
+};
