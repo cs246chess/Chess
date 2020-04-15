68a846a4239858c478533e10ef7b2c904836ed25
Atanyante <51100049+Atanyante@users.noreply.github.com>
2020-04-15 17:29:23 -0400

    Moved all piece classes into piece.h

--- piece.h
+++ piece.h
@@ -1,6 +1,7 @@
 #ifndef PIECE_H
 #define PIECE_H
 
+//base class
 class Piece {
   int row;
   int col;
@@ -8,8 +9,18 @@ class Piece {
 
   virtual bool validMove(int col, int row) = 0;
   virtual void move(int col, int row) = 0;
-  virtual int getRow() = 0;
-  virtual int getCol() = 0;
-  virtual char getColor() = 0;
+  int getRow();
+  int getCol();
+  char getColor();
+};
+
+class Pawn : public piece {
+  bool validMove(int col, int row) const override;
+  void move(int col, int row) const override;
+};
+
+Class Queen: Public Piece {
+    bool validMove(int row, int col) const override;
+    void move(int row, int col) override;
 };
 #endif
