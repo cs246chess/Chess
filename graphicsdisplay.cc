6d0504cc9b9d609d03e60aa5905e4af83a339e11
unknown <cs246chess>
2020-04-18 15:55:12 -0400

    Graphicsdisplay.cc

---
+++ graphicsdisplay.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+#include "graphicsdisplay.h"
+
+using namespace std;
+
+GraphicsDisplay::GraphicsDisplay(): {
+
+}
+
+
+void GraphicsDisplay::notify(Subject<Info, State> &whoNotified) {
+
+}
+
+
+
+
+GraphicsDisplay::~GraphicsDisplay() {}
