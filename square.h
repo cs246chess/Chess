8140bda2c845debab8de1f42351b5a933ddc4c5e
AkoDako <63671514+AkoDako@users.noreply.github.com>
2020-04-21 03:54:35 -0400

    colour not color noobs

--- square.h
+++ square.h
@@ -8,7 +8,7 @@
 class Square : public Subject, public Observer {
   const int r, c;
   Piece p;
-  Color colour;
+  Colour colour;
   bool isEmpty;
   Colour pieceColour; //colour of PIECE
   bool AttackedByWhite;
@@ -19,7 +19,7 @@ class Square : public Subject, public Observer {
   Square(int r, int c, Colour colour);
 
   void validMove(int row, int col);
-  void setPiece(Piece p, char pc, Color c);    // Place a piece of given colour here.
+  void setPiece(Piece p, char pc, Colour c);    // Place a piece of given colour here.
   void notify(Subject<Info, State> &whoFrom) override;// My neighbours will call this
   Info getInfo() const override;
 };
