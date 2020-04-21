eab2d24c770dad0cb22acb1a384a14d678736d79
Atanyante <51100049+Atanyante@users.noreply.github.com>
2020-04-21 03:10:25 -0400

    Made square.h public

--- square.h
+++ square.h
@@ -6,6 +6,7 @@
 #include "piece.h"
 
 class Square : public Subject, public Observer {
+  public:
   const int r, c;
   Piece p;
   Color colour;
@@ -14,18 +15,12 @@ class Square : public Subject, public Observer {
   bool AttackedByWhite;
   bool AttackedByBlack;
   // Add other private members if necessary
-
- public:
   Square(int r, int c, Colour colour);
 
   void validMove(int row, int col);
   void setPiece(Piece p, char pc, Color c);    // Place a piece of given colour here.
   void notify(Subject<Info, State> &whoFrom) override;// My neighbours will call this
   Info getInfo() const override;
-};
-
-class InvalidMove{
-
 };
 int abs(int x);
 #endif
