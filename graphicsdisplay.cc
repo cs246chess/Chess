f3de964477f01a92506adaf54e43922d2887e865
AkoDako <63671514+AkoDako@users.noreply.github.com>
2020-04-21 19:44:29 -0400

    gd

--- graphicsdisplay.cc
+++ graphicsdisplay.cc
@@ -35,62 +35,62 @@ void GraphicsDisplay::notify(Subject<Info, State> &whoNotified) {
   Piece piece = whoNotified.getInfo().piece;
   if (colour = Colour::Black) {
     if (piece == Piece::Pawn) {
-      gd.fillCircle(50 * row, 50 * col, 50, 50, Xwindow::Black);
+      gd.fillCircle(50 * row, 50 * col, 50, Xwindow::Black);
     }
     if (piece == Piece::Rook) {
-      gd.fillCircle(50 * row, 50 * col, 50, 50, Xwindow::Black);
-      gd.fillRectangle(50 * row, 50 * col, 50, 50, Xwindow::Black);
-      gd.fillCircle(50 * row, 50 * col, 50, 50, Xwindow::Black);
+      gd.fillCircle(50 * row, 50 * col, 50,  Xwindow::Black);
+      gd.fillRectangle(50 * row, 50 * col, 50, Xwindow::Black);
+      gd.fillCircle(50 * row, 50 * col, 50, Xwindow::Black);
     }
     if (piece == Piece::King) {
       gd.fillRectangle(50 * row, 50 * col, 50, 50, Xwindow::Black);
       gd.fillRectangle(50 * row, 50 * col, 10, 20, Xwindow::Black);
     }
     if (piece == Piece::Bishop) {
-      gd.fillCircle(50 * row, 50 * col, 50, 50, Xwindow::Black);
+      gd.fillCircle(50 * row, 50 * col, 50, Xwindow::Black);
       gd.fillRectangle(50 * row, 50 * col, 50, 50, Xwindow::Black);
-      gd.fillCircle(50 * row, 50 * col, 50, 50, Xwindow::Black);
-      gd.fillCircle(50 * row, 50 * col, 10, 20, Xwindow::Black);
+      gd.fillCircle(50 * row, 50 * col, 50, Xwindow::Black);
+      gd.fillCircle(50 * row, 50 * col, 10, Xwindow::Black);
     }
     if (piece == Piece::Knight) {
       gd.fillRectangle(50 * row, 50 * col, 50, 50, Xwindow::Black);
       gd.fillArc(50 * row, 50 * col, 50, 50, Xwindow::Black);
     }
     if (piece == Piece::Queen) {
-      gd.fillCircle(50 * row, 50 * col, 50, 50, Xwindow::Black);
+      gd.fillCircle(50 * row, 50 * col, 50, Xwindow::Black);
       gd.fillRectangle(50 * row, 50 * col, 50, 50, Xwindow::Black);
-      gd.fillCircle(50 * row, 50 * col, 50, 50, Xwindow::Black);
-      gd.fillCircle(50 * row, 50 * col, 10, 20, Xwindow::Black);
+      gd.fillCircle(50 * row, 50 * col, 50, Xwindow::Black);
+      gd.fillCircle(50 * row, 50 * col, 10, Xwindow::Black);
       gd.fillArc(50 * row, 50 * col, 50, 50, Xwindow::Black);
     }
   } else if (colour = Colour::White) {
     if (piece == Piece::Pawn) {
-      gd.fillCircle(50 * row, 50 * col, 50, 50, Xwindow::White);
+      gd.fillCircle(50 * row, 50 * col, 50, Xwindow::White);
     }
     if (piece == Piece::Rook) {
-      gd.fillCircle(50 * row, 50 * col, 50, 50, Xwindow::White);
+      gd.fillCircle(50 * row, 50 * col, 50, Xwindow::White);
       gd.fillRectangle(50 * row, 50 * col, 50, 50, Xwindow::White);
-      gd.fillCircle(50 * row, 50 * col, 50, 50, Xwindow::White);
+      gd.fillCircle(50 * row, 50 * col, 50, Xwindow::White);
     }
     if (piece == Piece::King) {
       gd.fillRectangle(50 * row, 50 * col, 50, 50, Xwindow::White);
       gd.fillRectangle(50 * row, 50 * col, 10, 20, Xwindow::White);
     }
     if (piece == Piece::Bishop) {
-      gd.fillCircle(50 * row, 50 * col, 50, 50, Xwindow::White);
+      gd.fillCircle(50 * row, 50 * col, 50, Xwindow::White);
       gd.fillRectangle(50 * row, 50 * col, 50, 50, Xwindow::White);
-      gd.fillCircle(50 * row, 50 * col, 50, 50, Xwindow::White);
-      gd.fillCircle(50 * row, 50 * col, 10, 20, Xwindow::White);
+      gd.fillCircle(50 * row, 50 * col, 50, Xwindow::White);
+      gd.fillCircle(50 * row, 50 * col, 10, Xwindow::White);
     }
     if (piece == Piece::Knight) {
       gd.fillRectangle(50 * row, 50 * col, 50, 50, Xwindow::White);
       gd.fillArc(50 * row, 50 * col, 50, 50, Xwindow::White);
     }
     if (piece == Piece::Queen) {
-      gd.fillCircle(50 * row, 50 * col, 50, 50, Xwindow::White);
+      gd.fillCircle(50 * row, 50 * col, 50,  Xwindow::White);
       gd.fillRectangle(50 * row, 50 * col, 50, 50, Xwindow::White);
-      gd.fillCircle(50 * row, 50 * col, 50, 50, Xwindow::White);
-      gd.fillCircle(50 * row, 50 * col, 10, 20, Xwindow::White);
+      gd.fillCircle(50 * row, 50 * col, 50,  Xwindow::White);
+      gd.fillCircle(50 * row, 50 * col, 10,  Xwindow::White);
       gd.fillArc(50 * row, 50 * col, 50, 50, Xwindow::White);
     }
   } else {
