84584d976d78e0b1b450376a888fd786c35b95e6
AkoDako <63671514+AkoDako@users.noreply.github.com>
2020-04-21 19:49:41 -0400

    gd

--- graphicsdisplay.cc
+++ graphicsdisplay.cc
@@ -33,7 +33,7 @@ void GraphicsDisplay::notify(Subject<Info, State> &whoNotified) {
   int col = whoNotified.getInfo().col;
   Colour colour = whoNotified.getInfo().colour;
   Piece piece = whoNotified.getInfo().piece;
-  if (colour = Colour::Black) {
+  if (colour == Colour::Black) {
     if (piece == Piece::Pawn) {
       gd.fillCircle(50 * row, 50 * col, 50, Xwindow::Black);
     }
@@ -54,16 +54,16 @@ void GraphicsDisplay::notify(Subject<Info, State> &whoNotified) {
     }
     if (piece == Piece::Knight) {
       gd.fillRectangle(50 * row, 50 * col, 50, 50, Xwindow::Black);
-      gd.fillArc(50 * row, 50 * col, 50, 50, 50, 60);
+      gd.fillArc(50 * row, 50 * col, 50, 50, 50, 60, Xwindow::Black);
     }
     if (piece == Piece::Queen) {
       gd.fillCircle(50 * row, 50 * col, 50, Xwindow::Black);
       gd.fillRectangle(50 * row, 50 * col, 50, 50, Xwindow::Black);
       gd.fillCircle(50 * row, 50 * col, 50, Xwindow::Black);
       gd.fillCircle(50 * row, 50 * col, 10, Xwindow::Black);
-      gd.fillArc(50 * row, 50 * col, 50, 50, 50, 60);
+      gd.fillArc(50 * row, 50 * col, 50, 50, 50, 60, Xwindow::Black);
     }
-  } else if (colour = Colour::White) {
+  } else if (colour == Colour::White) {
     if (piece == Piece::Pawn) {
       gd.fillCircle(50 * row, 50 * col, 50, Xwindow::White);
     }
@@ -84,14 +84,14 @@ void GraphicsDisplay::notify(Subject<Info, State> &whoNotified) {
     }
     if (piece == Piece::Knight) {
       gd.fillRectangle(50 * row, 50 * col, 50, 50, Xwindow::White);
-      gd.fillArc(50 * row, 50 * col, 50, 50, 50, 60);
+      gd.fillArc(50 * row, 50 * col, 50, 50, 50, 60, Xwindow::White);
     }
     if (piece == Piece::Queen) {
       gd.fillCircle(50 * row, 50 * col, 50,  Xwindow::White);
       gd.fillRectangle(50 * row, 50 * col, 50, 50, Xwindow::White);
       gd.fillCircle(50 * row, 50 * col, 50,  Xwindow::White);
       gd.fillCircle(50 * row, 50 * col, 10,  Xwindow::White);
-      gd.fillArc(50 * row, 50 * col, 50, 50, 50, 60);
+      gd.fillArc(50 * row, 50 * col, 50, 50, 50, 60, Xwindow::White);
     }
   } else {
     if (row % 2 == 0 && col % 2 == 0) {
